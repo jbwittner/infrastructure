68a3f2f9190a3345a6423f96105e682a303798b0
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2025-12-09 20:53:33 +0100

    Lock S-foils in attack position

--- terraform/authentik/main.tf
+++ terraform/authentik/main.tf
@@ -35,12 +35,16 @@ data "authentik_property_mapping_provider_scope" "openid" {
 }
 
 resource "authentik_provider_oauth2" "grafana-provider" {
-  name                    = "grafana-oauth2-provider"
-  client_id               = var.AUTHENTIK_GRAFANA_CLIENT_ID
-  client_secret           = var.AUTHENTIK_GRAFANA_CLIENT_SECRET
-  authorization_flow      = data.authentik_flow.grafana-authorization-flow.id
-  invalidation_flow       = data.authentik_flow.grafana-invalidation-flow.id
-  property_mappings       = [data.authentik_property_mapping_provider_scope.email.id, data.authentik_property_mapping_provider_scope.profile.id, data.authentik_property_mapping_provider_scope.openid.id]
+  name               = "grafana-oauth2-provider"
+  client_id          = var.AUTHENTIK_GRAFANA_CLIENT_ID
+  client_secret      = var.AUTHENTIK_GRAFANA_CLIENT_SECRET
+  authorization_flow = data.authentik_flow.grafana-authorization-flow.id
+  invalidation_flow  = data.authentik_flow.grafana-invalidation-flow.id
+  property_mappings = [
+    data.authentik_property_mapping_provider_scope.email.id,
+    data.authentik_property_mapping_provider_scope.profile.id,
+    data.authentik_property_mapping_provider_scope.openid.id
+  ]
   access_token_validity   = "minutes=5"
   refresh_token_threshold = "hours=1"
   refresh_token_validity  = "days=30"
