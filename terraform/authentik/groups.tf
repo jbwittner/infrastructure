70446bad792b26fb31e03b39458085753bb109a6
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2025-12-12 23:52:28 +0100

    Too lazy to write descriptive message

--- terraform/authentik/groups.tf
+++ terraform/authentik/groups.tf
@@ -1,23 +1,36 @@
+resource "authentik_group" "authentik_admins" {
+  name         = "authentik_admins"
+  users        = [authentik_user.jbwittner.id]
+  is_superuser = true
+}
+
 resource "authentik_group" "grafana-admins" {
   name         = "grafana_admins"
   users        = [authentik_user.jbwittner.id]
-  is_superuser = true
+  is_superuser = false
 }
 
 resource "authentik_group" "argocd-admins" {
   name         = "argocd_admins"
   users        = [authentik_user.jbwittner.id]
-  is_superuser = true
+  is_superuser = false
 }
 
 resource "authentik_group" "argocd-users" {
   name         = "argocd_users"
   users        = []
-  is_superuser = true
+  is_superuser = false
+}
+
+
+resource "authentik_group" "youtrack-users" {
+  name         = "youtrack_users"
+  users        = []
+  is_superuser = false
 }
 
 resource "authentik_group" "youtrack-admins" {
   name         = "youtrack_admins"
   users        = [authentik_user.jbwittner.id]
-  is_superuser = true
+  is_superuser = false
 }
