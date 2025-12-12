1b24f8b84ee5b95bd9cfcf6c59c3d71283b39d14
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2025-12-13 00:17:03 +0100

    I'll mention this again, if you're git-blaming this, don't come slap me personally. This code straight ported from another project and we WILL refactor this in the future. This is a temporary solution. OK I guess you can slap me for porting this as is, but still.

---
+++ terraform/gcp/backup/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "gcs" {
+    bucket = "f1ca7121a411ff04-terraform-backend"
+    prefix = "terraform/state/gcp/backup"
+  }
+}
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.13.0"
+    }
+  }
+}
